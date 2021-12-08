a4abefff2b991e6b9065a9d8ce6680e45a1980ee
Belvin Shandy Aurora <belshandy1209@gmail.com>
2021-12-08 16:43:00 +0700

    delete

--- arctic_geeks/arctic_geeks_app/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import JenisKomponen, Komponen, JenisInfo, InfoKomponen
-
-# Register your models here.
-admin.site.register(JenisKomponen)
-admin.site.register(Komponen)
-admin.site.register(JenisInfo)
-admin.site.register(InfoKomponen)
--- arctic_geeks/arctic_geeks_app/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ArcticGeeksAppConfig(AppConfig):
-    name = 'arctic_geeks_app'
