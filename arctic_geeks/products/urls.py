e539a21670f807b1c52966905f43845fa3023846
riandinp <riandinandaputra@yahoo.com>
2021-12-29 08:01:22 +0700

    update
    menambahkan fitur untuk menampilkan komponen berdasarkan jenis
    
    Co-Authored-By: Belvin Shandy Aurora <73571301+DizzyMeee@users.noreply.github.com>
    Co-Authored-By: Piuh13 <91477489+Piuh13@users.noreply.github.com>

--- arctic_geeks/products/urls.py
+++ arctic_geeks/products/urls.py
@@ -10,16 +10,24 @@ from products.views import (
     coolerComponent,
     caseComponent,
     fanComponent,
+    cpuComponentFiltered,
+    caseComponentFiltered,
+    moboComponentFiltered,
+    moboComponentFiltered1,
 )
 app_name = "products"
 urlpatterns = [
     path('gpu/', gpuComponent, name='gpu'),
     path('cpu/', cpuComponent, name='cpu'),
+    path('cpu/<str:socket>', cpuComponentFiltered, name='cpuFilter'),
     path('mobo/', moboComponent, name='mobo'),
+    path('mobo/<str:ff>/<str:socket>', moboComponentFiltered, name='moboFilter'),
+    path('mobo/<str:component>', moboComponentFiltered1, name='moboFilter_satu'),
     path('ram/', ramComponent, name='ram'),
     path('disk/', diskComponent, name='disk'),
     path('psu/', psuComponent, name='psu'),
     path('cooler/', coolerComponent, name='cooler'),
     path('case/', caseComponent, name='case'),
+    path('case/<str:ff>', caseComponentFiltered, name='caseFilter'),
     path('fan/', fanComponent, name='fan'),
 ]
--- arctic_geeks/products/views.py
+++ arctic_geeks/products/views.py
@@ -11,13 +11,57 @@ def gpuComponent(request):
     gpuComponents = GPU.objects.all()
     return render(request, 'products/gpuBrowse.html', {'komponenGPU' : gpuComponents})
 
+def cpuComponentFiltered(request,socket):
+    cpuComponents = CPU.objects.filter(socket=socket)
+    return render(request, 'products/cpuBrowse.html', {'komponenCPU' : cpuComponents})
+
 def cpuComponent(request):
     cpuComponents = CPU.objects.all()
     return render(request, 'products/cpuBrowse.html', {'komponenCPU' : cpuComponents})
     
-
 def moboComponent(request):
     moboComponents = Motherboard.objects.all()
+    return render(request, 'products/moboBrowse.html', {'komponenMobo' : moboComponents})
+
+def moboComponentFiltered(request,ff,socket):
+    list_Case = list(Case.objects.values_list('form_factor'))
+    for value in list_Case:
+        if ff in value:
+            if ff == 'ITX':
+                moboComponents = Motherboard.objects.filter(form_factor=ff,socket=socket)
+            elif ff == 'M-ATX':
+                moboComponents = Motherboard.objects.filter(form_factor__in=['M-ATX',ff],socket=socket)
+            else:
+                moboComponents = Motherboard.objects.filter(socket=socket)
+            return render(request, 'products/moboBrowse.html', {'komponenMobo' : moboComponents})
+    moboComponents = Motherboard.objects.all()
+    return render(request, 'products/moboBrowse.html', {'komponenMobo' : moboComponents})
+
+def moboComponentFiltered1(request,component):
+    list_Case = list(Case.objects.values_list('form_factor'))
+    list_CPU = list(CPU.objects.values_list('socket'))
+    for value in list_Case:
+        if component in value:
+            if component == 'ITX':
+                moboComponents = Motherboard.objects.filter(form_factor=component)
+            elif component == 'M-ATX':
+                moboComponents = Motherboard.objects.filter(form_factor__in=['M-ATX',component])
+            else:
+                moboComponents = Motherboard.objects.all()
+            return render(request, 'products/moboBrowse.html', {'komponenMobo' : moboComponents})
+
+    for value in list_CPU:
+        if component in value:
+            print('KetemuCPU')
+            moboComponents = Motherboard.objects.filter(socket=component)
+            return render(request, 'products/moboBrowse.html', {'komponenMobo' : moboComponents})
+
+    moboComponents = Motherboard.objects.all()
+    return render(request, 'products/moboBrowse.html', {'komponenMobo' : moboComponents})
+
+def moboComponentFiltered2(request,socket):
+    moboComponents = Motherboard.objects.filter(socket=socket)
+    print('socket')
     return render(request, 'products/moboBrowse.html', {'komponenMobo' : moboComponents})
 
 def ramComponent(request):
@@ -37,6 +81,20 @@ def coolerComponent(request):
     return render(request, 'products/coolerBrowse.html', {'komponenCooler' : coolerComponents})
 
 def caseComponent(request):
+    caseComponents = Case.objects.all()
+    return render(request, 'products/caseBrowse.html', {'komponenCase' : caseComponents})
+
+def caseComponentFiltered(request,ff):
+    list_Motherboard = list(Motherboard.objects.values_list('form_factor'))
+    for value in list_Motherboard:
+        if ff in value:
+            if ff == 'ITX':
+                caseComponents = Case.objects.filter(form_factor=ff)
+            elif ff == 'M-ATX':
+                caseComponents = Case.objects.filter(form_factor__in=['M-ATX',ff])
+            else:
+                caseComponents = Case.objects.all()
+            return render(request, 'products/caseBrowse.html', {'komponenCase' : caseComponents})
     caseComponents = Case.objects.all()
     return render(request, 'products/caseBrowse.html', {'komponenCase' : caseComponents})
 
