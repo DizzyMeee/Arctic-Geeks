14eaeb1bd87c06c89f1d5cbbd4a8c6222bdd4380
riandinp <riandinandaputra@yahoo.com>
2021-12-30 05:44:34 +0700

    tambah produk

--- arctic_geeks/products/views.py
+++ arctic_geeks/products/views.py
@@ -8,19 +8,19 @@ from django.shortcuts import render, redirect
 from .models import GPU, CPU, Motherboard, RAM, Disk, PowerSupply, Cooler, Case, Fan
 
 def gpuComponent(request):
-    gpuComponents = GPU.objects.all()
+    gpuComponents = GPU.objects.all().order_by('nama_komponen')
     return render(request, 'products/gpuBrowse.html', {'komponenGPU' : gpuComponents})
 
 def cpuComponentFiltered(request,socket):
-    cpuComponents = CPU.objects.filter(socket=socket)
+    cpuComponents = CPU.objects.filter(socket=socket).order_by('nama_komponen')
     return render(request, 'products/cpuBrowse.html', {'komponenCPU' : cpuComponents})
 
 def cpuComponent(request):
-    cpuComponents = CPU.objects.all()
+    cpuComponents = CPU.objects.all().order_by('nama_komponen')
     return render(request, 'products/cpuBrowse.html', {'komponenCPU' : cpuComponents})
     
 def moboComponent(request):
-    moboComponents = Motherboard.objects.all()
+    moboComponents = Motherboard.objects.all().order_by('nama_komponen')
     return render(request, 'products/moboBrowse.html', {'komponenMobo' : moboComponents})
 
 def moboComponentFiltered(request,ff,socket):
@@ -28,13 +28,13 @@ def moboComponentFiltered(request,ff,socket):
     for value in list_Case:
         if ff in value:
             if ff == 'ITX':
-                moboComponents = Motherboard.objects.filter(form_factor=ff,socket=socket)
+                moboComponents = Motherboard.objects.filter(form_factor=ff,socket=socket).order_by('nama_komponen')
             elif ff == 'M-ATX':
-                moboComponents = Motherboard.objects.filter(form_factor__in=['ITX',ff],socket=socket)
+                moboComponents = Motherboard.objects.filter(form_factor__in=['ITX',ff],socket=socket).order_by('nama_komponen')
             else:
-                moboComponents = Motherboard.objects.filter(socket=socket)
+                moboComponents = Motherboard.objects.filter(socket=socket).order_by('nama_komponen')
             return render(request, 'products/moboBrowse.html', {'komponenMobo' : moboComponents})
-    moboComponents = Motherboard.objects.all()
+    moboComponents = Motherboard.objects.all().order_by('nama_komponen')
     return render(request, 'products/moboBrowse.html', {'komponenMobo' : moboComponents})
 
 def moboComponentFiltered1(request,component):
@@ -43,45 +43,45 @@ def moboComponentFiltered1(request,component):
     for value in list_Case:
         if component in value:
             if component == 'ITX':
-                moboComponents = Motherboard.objects.filter(form_factor=component)
+                moboComponents = Motherboard.objects.filter(form_factor=component).order_by('nama_komponen')
             elif component == 'M-ATX':
-                moboComponents = Motherboard.objects.filter(form_factor__in=['M-ATX',component])
+                moboComponents = Motherboard.objects.filter(form_factor__in=['M-ATX',component]).order_by('nama_komponen')
             else:
-                moboComponents = Motherboard.objects.all()
+                moboComponents = Motherboard.objects.all().order_by('nama_komponen')
             return render(request, 'products/moboBrowse.html', {'komponenMobo' : moboComponents})
 
     for value in list_CPU:
         if component in value:
             print('KetemuCPU')
-            moboComponents = Motherboard.objects.filter(socket=component)
+            moboComponents = Motherboard.objects.filter(socket=component).order_by('nama_komponen')
             return render(request, 'products/moboBrowse.html', {'komponenMobo' : moboComponents})
 
-    moboComponents = Motherboard.objects.all()
+    moboComponents = Motherboard.objects.all().order_by('nama_komponen')
     return render(request, 'products/moboBrowse.html', {'komponenMobo' : moboComponents})
 
 def moboComponentFiltered2(request,socket):
-    moboComponents = Motherboard.objects.filter(socket=socket)
+    moboComponents = Motherboard.objects.filter(socket=socket).order_by('nama_komponen')
     print('socket')
     return render(request, 'products/moboBrowse.html', {'komponenMobo' : moboComponents})
 
 def ramComponent(request):
-    ramComponents = RAM.objects.all()
+    ramComponents = RAM.objects.all().order_by('nama_komponen')
     return render(request, 'products/ramBrowse.html', {'komponenRAM' : ramComponents})
 
 def diskComponent(request):
-    diskComponents = Disk.objects.all()
+    diskComponents = Disk.objects.all().order_by('nama_komponen')
     return render(request, 'products/diskBrowse.html', {'komponenDisk' : diskComponents})
 
 def psuComponent(request):
-    psuComponents = PowerSupply.objects.all()
+    psuComponents = PowerSupply.objects.all().order_by('nama_komponen')
     return render(request, 'products/psuBrowse.html', {'komponenPSU' : psuComponents})
 
 def coolerComponent(request):
-    coolerComponents = Cooler.objects.all()
+    coolerComponents = Cooler.objects.all().order_by('nama_komponen')
     return render(request, 'products/coolerBrowse.html', {'komponenCooler' : coolerComponents})
 
 def caseComponent(request):
-    caseComponents = Case.objects.all()
+    caseComponents = Case.objects.all().order_by('nama_komponen')
     return render(request, 'products/caseBrowse.html', {'komponenCase' : caseComponents})
 
 def caseComponentFiltered(request,ff):
@@ -89,17 +89,17 @@ def caseComponentFiltered(request,ff):
     for value in list_Motherboard:
         if ff in value:
             if ff == 'ATX':
-                caseComponents = Case.objects.filter(form_factor=ff)
+                caseComponents = Case.objects.filter(form_factor=ff).order_by('nama_komponen')
             elif ff == 'M-ATX':
-                caseComponents = Case.objects.filter(form_factor__in=['ATX',ff])
+                caseComponents = Case.objects.filter(form_factor__in=['ATX',ff]).order_by('nama_komponen')
             else:
-                caseComponents = Case.objects.all()
+                caseComponents = Case.objects.all().order_by('nama_komponen')
             return render(request, 'products/caseBrowse.html', {'komponenCase' : caseComponents})
-    caseComponents = Case.objects.all()
+    caseComponents = Case.objects.all().order_by('nama_komponen')
     return render(request, 'products/caseBrowse.html', {'komponenCase' : caseComponents})
 
 def fanComponent(request):
-    fanComponents = Fan.objects.all()
+    fanComponents = Fan.objects.all().order_by('nama_komponen')
     return render(request, 'products/fanBrowse.html', {'komponenFan' : fanComponents})
 
 
